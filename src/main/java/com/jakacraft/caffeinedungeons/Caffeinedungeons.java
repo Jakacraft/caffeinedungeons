cbde94e9e96acab4e3b589500944c8020089b26c
Jakacraft <jakeclarke814@gmail.com>
2025-05-25 18:37:00 +1000

    Made it so that minecraft starts Setup Workspace Made a icon

---
+++ src/main/java/com/jakacraft/caffeinedungeons/Caffeinedungeons.java
@@ -0,0 +1,10 @@
+package com.jakacraft.caffeinedungeons;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Caffeinedungeons implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
